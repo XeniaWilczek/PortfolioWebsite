79ed6eca48b8f1487db10f150185bfb4a81fdbb2
Xenia Wilczek <xenia.wilczek@gmx.de>
2026-06-25 09:59:42 +0200

    Adapt impressum.html

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/PortfolioWebsite/",
   plugins: [tailwindcss()],
+  build: {
+    rolldownOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        impressum: resolve(__dirname, "impressum.html"),
+      },
+    },
+  },
 });
